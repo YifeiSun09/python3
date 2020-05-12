40ace917e2c7ae9dfb46ce05ead355a5f53bdc34
FrancisX <1340058590@qq.com>
2020-05-12 13:29:40 +0800

    input and output

---
+++ codes/0512_python_input_output.py
@@ -0,0 +1,13 @@
+#coding=utf-8
+
+print('Hello,World!')
+print('Hello','World!')
+print('2','B','No.','1')
+print()
+print(1)
+print(2**2)
+print()
+
+print('Robot: What\'s your name?')
+name = input('My Name is ')
+print('Robot: Hello,'+name+'! Welcome.')
