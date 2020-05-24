230bd72c518e8c46f9e928da3e572b38a5739a79
FrancisX <1340058590@qq.com>
2020-05-24 22:03:26 +0800

    单元测试

---
+++ codes/assert.py
@@ -0,0 +1,4 @@
+#coding:utf8
+# 调试
+import logging
+assert 0 == 1, logging.info('Oh No.')
---
+++ codes/custom_unittest.py
@@ -0,0 +1,35 @@
+#coding:utf8
+import unittest
+from custom_dict import Dict
+
+class TestDict(unittest.TestCase):
+    def test_init(self):
+        d = Dict(a = 1, b = 'test')
+        self.assertEqual(d.a, 1)
+        self.assertEqual(d.b, 'test')
+        self.assertTrue(isinstance(d, dict))
+    
+    def test_key(self):
+        d = Dict()
+        d['key'] = 'value'
+        self.assertEqual(d.key, 'value')
+    
+    def test_attr(self):
+        d = Dict()
+        d.age = 25
+        self.assertTrue('age' in d)
+        self.assertEqual(d['age'],25)
+    
+    def test_keyerror(self):
+        d = Dict()
+        with self.assertRaises(KeyError):
+            value = d['empty']
+    
+    def test_attrerror(self):
+        d = Dict()
+        with self.assertRaises(AttributeError):
+            value = d.empty
+
+# 直接调用unittest.main
+if __name__ == '__main__':
+    unittest.main()
